e2c582732027bc63e77aa0df425b666ab8eae643
peter ji <hyeongjun9@gmail.com>
2023-09-20 14:07:41 -0700

    beer controller

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -2,4 +2,17 @@ class BeersController < ApplicationController
   def index 
     beers = Beer.all
     render json: beers
+  end
+
+  def show
+    beer = Beer.find(params[:id])
+    render json: beer
+  end
+
+  private
+
+  def beer_params
+    params.require(:beer).permit(:name, :style, :abv, :description, :image_url)
+  end
+
 end
--- spec/requests/beers_spec.rb
+++ spec/requests/beers_spec.rb
@@ -2,6 +2,35 @@ require 'rails_helper'
 
 RSpec.describe "Beers", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of beers" do
+      Beer.create(
+        name: "Modelo Especial",
+        style: "Pilsner-style Lager",
+        abv: 4.4,
+        description: "An orange blossom honey aroma with a hint of herb.",
+        image_url: "https://www.modelousa.com/cdn/shop/files/Especial-02_0_1024x1024.jpg?v=1638951577"
+      )
+
+      get '/beers'
+      beer = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(beer.length).to eq 1
+    end
   end
+
+  describe "GET /show/:id" do
+    it "shows a single beer" do
+      beer1 = Beer.create(
+        name: "Modelo Especial",
+        style: "Pilsner-style Lager",
+        abv: 4.4,
+        description: "An orange blossom honey aroma with a hint of herb.",
+        image_url: "https://www.modelousa.com/cdn/shop/files/Especial-02_0_1024x1024.jpg?v=1638951577"
+      )
+
+      get "beers/#{beer1.id}"
+      expect(response).to have_http_status(200)
+    end
+  end
+
 end
