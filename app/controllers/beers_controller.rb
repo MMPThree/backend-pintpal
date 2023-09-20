3994325208da2fa70503cec2d48b89c46e363595
megandee <meganderisi@gmail.com>
2023-09-20 16:46:23 -0500

    finished rspecendpoints

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -9,6 +9,35 @@ class BeersController < ApplicationController
     render json: beer
   end
 
+  def create 
+    beer = Beer.create(params[:id])
+    beer.update(beer_params)
+    if beer.valid?
+      render json: beer
+    else
+      render json: beer.errors, status: 422
+    end
+  end
+
+  def update
+    beer = Beer.find(params[:id])
+    beer.update(beer_params)
+    if beer.valid?
+      render json: beer
+    else
+      render json: beer.errors, status: 422
+    end
+  end
+
+  def destroy 
+    beer = Beer.find(params[:id])
+    if beer.destroy
+      render json: beer
+    else
+      render json: beer.errors, status: 422
+    end
+  end
+
   private
 
   def beer_params
--- spec/requests/beers_spec.rb
+++ spec/requests/beers_spec.rb
@@ -27,9 +27,74 @@ RSpec.describe "Beers", type: :request do
         description: "An orange blossom honey aroma with a hint of herb.",
         image_url: "https://www.modelousa.com/cdn/shop/files/Especial-02_0_1024x1024.jpg?v=1638951577"
       )
-
-      get "beers/#{beer1.id}"
+      p beer1 
+      get "/beers/#{beer1.id}"
       expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "POST /create" do
+    it "creates a beer" do 
+    beer_params = {
+      beer: {
+        name: "Modelo Especial",
+        style: "Pilsner-style Lager",
+        abv: 4.4,
+        description: "An orange blossom honey aroma with a hint of herb.",
+        image_url: "https://www.modelousa.com/cdn/shop/files/Especial-02_0_1024x1024.jpg?v=1638951577"
+      }
+    } 
+    post "/beers", params: beer_params
+    JSON.parse(response.body)
+    beer = Beer.first
+    expect(response).to have_http_status(200)
+    end 
+  end
+
+  describe "PATCH /update" do
+    it "it updates a beer" do
+      beer_params = {
+        beer: {
+          name: "Modelo Especial",
+          style: "Pilsner-style Lager",
+          abv: 4.4,
+          description: "An orange blossom honey aroma with a hint of herb.",
+          image_url: "https://www.modelousa.com/cdn/shop/files/Especial-02_0_1024x1024.jpg?v=1638951577"
+        }
+      }
+      post "/beers", params: beer_params
+
+      beer = Beer.first 
+      update_beer_params = {
+        beer: {
+          name: "Modelo Especial",
+          style: "Pilsner-style Lager",
+          abv: 4.4,
+          description: "An orange blossom honey aroma with a hint of herb.",
+          image_url: "https://www.modelousa.com/cdn/shop/files/Especial-02_0_1024x1024.jpg?v=1638951577"
+      }
+    }
+    end
+  end 
+
+  describe "DELETE /destroy" do
+    it "it deletes a beer" do
+      beer_params = {
+        beer: {
+          name: "Modelo Especial",
+          style: "Pilsner-style Lager",
+          abv: 4.4,
+          description: "An orange blossom honey aroma with a hint of herb.",
+          image_url: "https://www.modelousa.com/cdn/shop/files/Especial-02_0_1024x1024.jpg?v=1638951577"
+        }
+      }
+
+      post '/beers', params: beer_params
+      beer = Beer.first
+      delete "/beers/#{beer.id}"
+      expect(response).to have_http_status(200)
+      beers = Beer.all
+      expect(beers).to be_empty 
     end
   end
 
