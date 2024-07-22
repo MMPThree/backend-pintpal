91afc5c6185672c4a0040eab58e71dcca688d4c3
Mikechavo <mchavez76@yahoo.com>
2024-07-21 19:06:54 -0500

    fixing render build file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,19 +1,6 @@
-#!/bin/sh
 set -o errexit
-set -o nounset
-set -o pipefail
 
-# Install gems
 bundle install
-
-# Precompile assets
 bundle exec rake assets:precompile
-
-# Clean up old assets
 bundle exec rake assets:clean
-
-# Run database migrations
 bundle exec rake db:migrate
-
-# Optionally, you might want to clear old logs
-# bundle exec rake log:clear
